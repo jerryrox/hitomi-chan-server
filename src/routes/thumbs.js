6972b425f1c172c3a3d1f9b5ae0f2121c627aba9
jerryrox <jerryrox@live.co.kr>
2018-06-19 21:18:47 +1200

    Some changes

--- src/routes/thumbs.js
+++ src/routes/thumbs.js
@@ -3,10 +3,11 @@ const request = require("request");
 const router = express.Router();
 
 const response = require("../response");
+const { requestPageInfo } = require("../webClient");
 const { url } = require("../hitomi-chan-utility");
 const { isParamIdValid } = require("../utils");
 
-router.get("/big/:id/:fileName", (req, res) => {
+router.get("/big/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const fileName = req.params.fileName;
 
@@ -15,7 +16,14 @@ router.get("/big/:id/:fileName", (req, res) => {
         return;
     }
 
-    request(url.getBigThumbUrl(id, fileName)).pipe(res);
+    requestPageInfo(id, (pages) => {
+        if(pages === null || pages.length === 0) {
+            res.status(404).send();
+            return;
+        }
+
+        request(url.getBigThumbUrl(id, pages[0].name)).pipe(res);
+    });
 });
 
 router.get("/small/:id/:fileName", (req, res) => {
--- src/webClient/index.js
+++ src/webClient/index.js
@@ -1,7 +1,22 @@
 const axios = require("axios");
 
+const { url, util, PageInfo } = require("../hitomi-chan-utility");
 
+/**
+ * Requests for a gallery's page info array.
+ * @param {(pages: Array<PageInfo>) => void} callback
+ */
+function requestPageInfo(galleryId, callback) {
+    let target = url.getPageInfoUrl(galleryId);
+    axios.get(target)
+        .then(res => {
+            callback(util.parsePageInfo(res.data));
+        })
+        .catch(err => {
+            callback(null);
+        });
+}
 
 module.exports = {
-
+    requestPageInfo
 };
