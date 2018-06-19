73207df9e5f0ac01b5527dc6ef33dd092f91da56
jerryrox <jerryrox@live.co.kr>
2018-06-19 20:02:11 +1200

    Added "original" route

--- src/routes/original.js
+++ src/routes/original.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const response = require("../response");
+const { url } = require("../hitomi-chan-utility");
+
+router.get("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    // Invalid param
+    if(id <= 0 || isNaN(id)) {
+        res.send(response.getFailResponse("id must be an integer value!"));
+        return;
+    }
+
+    res.send(response.getSuccessResponse({
+        link: url.getGalleryUrl(req.params.id)
+    }));
+});
+
 module.exports = router;
